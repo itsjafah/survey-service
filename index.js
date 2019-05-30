90be3d4ac0632dba5be6bc0388dce6fbf468cc94
github username <github email address>
2019-05-30 13:17:09 -0400

    passport and strategy added, along with credentials

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const passport = require("passport");
+// passport is the library that handles authentication
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// GoogleStrategy instructs passport how to handle authentication requests specifically for Google
 const app = express();
 
-// this is a route handler with express
+// passport.use is like a generic register. We pass the GoogleStrategy in as a parameter since that is the specific authentication we are making in this case.
+
+// the new GoogleStrategy constructor function below creates a new instance of the google passport Strategy. Essentially it tells the app to authenticate users with google. We pass a parameter to the function that instructs the GoogleStrategy how to authenticate users in our application.
+
+// client ID 566592909212-ppkh07mf9g3fpruhg5tvp67sei6gv8cg.apps.googleusercontent.com
+// client secret IIfe7Knftuw0YVrr5ODpmf7z
+
+passport.use(new GoogleStrategy());
+
+// below is an example of a route handler with express
 
 // the request handler has an argument of an arrow function that is automatically called by Express whenever there is a get request sent to the '/' route
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,3 +29,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 // this listen method is Express telling Node to listen for all requests coming into port 5000
+
+// http://localhost:5000/auth/google/callback
